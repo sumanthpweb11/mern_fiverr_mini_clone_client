d4d05f5596de293c3658aa35a04ba7f5b394bc05
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-03-11 22:09:05 +0530

    c

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   // withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("currentUser")}`,
+  },
 });
 
 export default newRequest;
