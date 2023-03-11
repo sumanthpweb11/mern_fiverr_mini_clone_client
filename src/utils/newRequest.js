48e9cb8a45fff08d6c6f73c8ea5b06d83246abd5
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-03-11 20:19:40 +0530

    c

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: import.meta.env.REACT_APP_DEV,
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default newRequest;
